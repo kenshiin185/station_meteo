17605284a133ef43126292d7f5b7aadbf65699bd
kenshiin185 <bruno.castagna@live.fr>
2021-04-26 16:29:10 +0200

    météo fonctionnelle

---
+++ main.js
@@ -0,0 +1,57 @@
+//alert('test');
+
+const api = {
+    key: "ef70de7a3866c7bab2565ebfcf518355",
+    baseurl: "https://api.openweathermap.org/data/2.5/"
+
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode ==13) {
+        getResults(searchbox.value);
+        console.log(searchbox.value);
+    }
+}
+
+function getResults(query) {
+    fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=ef70de7a3866c7bab2565ebfcf518355&lang=Fr`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults(weather) {
+    console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let today = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(today);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+
+    let minMax = document.querySelector('.min-max');
+    minMax.innerText = `${weather.main.temp_min}°C / ${weather.main.temp_max}°C`;
+
+}
+
+function dateBuilder(d) {
+
+    let months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+    let days = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
